cac930df5ce9443bfd787f5501f744d1135d9034
Kaoru <dev@kaoruk.com>
2013-09-18 17:26:08 -0500

    Finish twitter

--- clashing-hashes.rb
+++ clashing-hashes.rb
@@ -5,9 +5,11 @@ require 'sinatra/reloader'
 
 module Twitter
   @hash_list = {
-    "makersquare" => 5,
-    "board" => 1
+    "#makersquare" => 5,
+    "#board" => 1
   }
+
+  @tweet_list = [];
 
   def self.compare(x, y)
     return "Sorry no such hash" if @hash_list[x] == nil && @hash_list[y] == nil
@@ -15,6 +17,7 @@ module Twitter
     return y if @hash_list[y] > @hash_list[x]
     return "Tied!" if @hash_list[x] == @hash_list[y]
   end
+
 
   def self.add_hash(name)
     if @hash_list[name] != nil
@@ -27,11 +30,54 @@ module Twitter
   def self.hash_list
     @hash_list
   end
+
+  def self.tweet_list
+    @tweet_list
+  end
+
+  def self.add_tweet(name, msg)
+    obj = Tweet.new(name, msg)
+    @tweet_list.push(obj)
+  end
+
+  def self.search_hash(msg)
+    words = msg.split(" ")
+    hash = []
+    words.each {|word|
+      hash.push(word) if word.include?("#")
+    }
+    if hash != []
+      hash.each {|hash|
+        add_hash(hash)
+      }
+    end
+  end
+
+  class Tweet
+    attr_accessor :name, :msg
+
+    def initialize(name, msg)
+      @name = name
+      @msg = msg
+    end
+  end
 end
+
+Twitter.add_tweet("Bob", "yay baby");
+Twitter.add_tweet("smith", "yay baby");
 
 
 get '/' do
   @hash_list = Twitter.hash_list
+  @tweet_list = Twitter.tweet_list
+  erb :index
+end
+
+post '/' do
+  Twitter.search_hash(params[:msg])
+  Twitter.add_tweet(params[:name], params[:msg])
+  @hash_list = Twitter.hash_list
+  @tweet_list = Twitter.tweet_list
   erb :index
 end
 
