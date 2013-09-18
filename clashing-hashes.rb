b3a7bedfbd9604cbb8c14843f0fb94519d95bac6
Kaoru <dev@kaoruk.com>
2013-09-18 16:31:21 -0500

    Add tweets, compare tweets

--- clashing-hashes.rb
+++ clashing-hashes.rb
@@ -2,18 +2,46 @@ require 'rubygems'
 require 'bundler/setup'
 require 'sinatra'
 require 'sinatra/reloader'
-require_relative 'lib/twitter.rb'
-include Twitter
 
-puts Twitter
+module Twitter
+  @hash_list = {
+    "makersquare" => 5,
+    "board" => 1
+  }
+
+  def self.compare(x, y)
+    return "Sorry no such hash" if @hash_list[x] == nil && @hash_list[y] == nil
+    return x if @hash_list[x] > @hash_list[y]
+    return y if @hash_list[y] > @hash_list[x]
+    return "Tied!" if @hash_list[x] == @hash_list[y]
+  end
+
+  def self.add_hash(name)
+    if @hash_list[name] != nil
+      @hash_list[name] += 1
+    else
+      @hash_list[name] = 1
+    end
+  end
+
+  def self.hash_list
+    @hash_list
+  end
+end
+
 
 get '/' do
-
-  @show_desc = nil
+  @hash_list = Twitter.hash_list
   erb :index
 end
 
-post '/twitter' do
+post '/compare' do
+  @results = Twitter.compare(params[:first_hash], params[:second_hash])
   erb :results
+end
 
+post '/tweet-add' do
+  Twitter.add_hash(params[:first_hash])
+  Twitter.add_hash(params[:second_hash])
+  erb :add
 end
--- lib/twitter.rb
+++ lib/twitter.rb
@@ -1,8 +1,18 @@
 module Twitter
   class Tweet
-    attr_accessor :name
+    attr_accessor :hash_list, :count, :name
+    
     def initialize(name)
       @name = name
+      @count = 1
+    end
+
+    def add_hash
+      @count += 1
+    end
+
+    def countUp
+      
     end
   end
 end
